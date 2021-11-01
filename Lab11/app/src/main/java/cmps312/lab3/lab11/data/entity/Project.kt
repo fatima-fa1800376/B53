b118109805942d32f15e4d920000e9853d8cf26b
WaleedBinOwais <64152361+WaleedBinOwais@users.noreply.github.com>
2021-11-01 15:03:13 +0300

    update

---
+++ Lab11/app/src/main/java/cmps312/lab3/lab11/data/entity/Project.kt
@@ -0,0 +1,11 @@
+package cmps312.lab3.lab11.data.entity
+
+import com.google.firebase.firestore.DocumentId
+
+data class Project(
+
+    var projectId: String = "",
+    var name: String? = "",
+
+
+)
---
+++ Lab11/app/src/main/java/cmps312/lab3/lab11/data/entity/Todo.kt
@@ -0,0 +1,11 @@
+package cmps312.lab3.lab11.data.entity
+
+data class Todo(
+    var title: String? = "",
+
+    var priority: String? = "",
+
+
+    var projectId: String? = "",
+    var todoId: String? = "",
+)
