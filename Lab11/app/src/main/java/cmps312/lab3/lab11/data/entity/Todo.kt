04249597ef2a28f5b66ac80ed44171fa1f4525f2
fatima-fa1800376 <88963960+fatima-fa1800376@users.noreply.github.com>
2021-11-08 14:31:46 +0300

    Revert "."
    This reverts commit 3b47e3939770780794fe6ab78d4d7122c67ec735.

--- Lab11/app/src/main/java/cmps312/lab3/lab11/data/entity/Todo.kt
+++ Lab11/app/src/main/java/cmps312/lab3/lab11/data/entity/Todo.kt
@@ -2,7 +2,10 @@ package cmps312.lab3.lab11.data.entity
 
 data class Todo(
     var title: String? = "",
+
     var priority: String? = "",
+
+
     var projectId: String? = "",
     var todoId: String? = "",
 )
